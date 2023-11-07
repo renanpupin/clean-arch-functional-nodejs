62c663fe4a0c024c47c27d9439566c84f4b0713a
Renan Pupin <renan.pupin@gmail.com>
2023-11-07 17:55:13 -0300

    ops: fix order for client

--- src/drivers/db/prismaDb.ts
+++ src/drivers/db/prismaDb.ts
@@ -1,4 +1,5 @@
 import {PrismaClient} from '@prisma/client'
 
-export type PrismaDbType = typeof PrismaClient
 export const PrismaDb = new PrismaClient()
+
+export type PrismaDbType = typeof PrismaClient
